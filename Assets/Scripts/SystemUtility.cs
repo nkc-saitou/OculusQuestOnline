9b5e4a2db27dbe9a69d57d38781d9b42e3f4de77
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2019-09-10 16:58:33 +0900

    UniRx

---
+++ Assets/Scripts/SystemUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class SystemUtility : MonoBehaviour
+{
+    void Start()
+    {
+        OVRManager.display.displayFrequency = 72f; //72Hzモードに変更
+
+        XRSettings.eyeTextureResolutionScale = 1.25f;
+
+        //焦点以外の周囲を圧縮する
+        OVRManager.fixedFoveatedRenderingLevel = OVRManager.FixedFoveatedRenderingLevel.Medium;
+
+        //パフォーマンスレベルの変更 Default=2 0~3,低~高 とパフォーマンスが変わる
+        //3にすると高クロックになり著しくバッテリー消費と発熱を起こす．
+        OVRManager.cpuLevel = 2;
+        OVRManager.gpuLevel = 2;
+
+
+    }
+}
