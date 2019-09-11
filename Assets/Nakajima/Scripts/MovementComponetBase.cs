88dc374b79915be1e1e986aca2bd3aa89481cd10
中島和也 <nkc.kazuya.nakajima@gmail.com>
2019-09-11 13:35:34 +0900

    スティックでの移動の実装

--- Assets/Nakajima/Scripts/MovementComponetBase.cs
+++ Assets/Nakajima/Scripts/MovementComponetBase.cs
@@ -19,30 +19,30 @@ namespace Nakajima.Movement
             // 傾きで移動
             MOVE_INCLINATION,
         }
-        [Header("移動手段")]
+        [Header("＜移動手段＞")]
         public MovementState movementState;
 
+
         // 移動する力
-        [Header("移動力")]
-        public Vector3 velocity;
-        // 移動方向
-        protected Vector3 moveVec;
-        // 現在の方向
-        [Header("現在の方向")]
-        public Vector3 movementDir;
+        private Vector3 velocity;
+        public Vector3 Velocity {
+            get { return velocity; }
+        }
 
         // 入力方向
-        [Header("入力が検知された方向")]
+        [Header("＜入力が検知された方向＞")]
         public Vector3 inputVec;
                      
         // 移動速度(最小、最大)
-        [Header("最低スピード")]
-        public float minSpeed;
-        [Header("最速スピード")]
-        public float maxSpeed;
+        [Header("＜最低スピード＞")]
+        public float minSpeed = 10;
+        [Header("＜最速スピード＞")]
+        public float maxSpeed = 60;
         // 加速力
-        [Header("加速スピード")]
-        public float accelSpeed;
+        [Header("＜加速スピード＞")]
+        public float accelSpeed = 0.5f;
+        // 現在のスピード
+        private float currentSpeed;
 
         /// <summary>
         /// 初期化
@@ -58,6 +58,38 @@ namespace Nakajima.Movement
         void Update()
         {
 
+        }
+
+        /// <summary>
+        /// 移動力を加える
+        /// </summary>
+        /// <param name="_vec"></param>
+        public void AddInputVector(Vector3 _vec)
+        {
+            // 入力があるなら加速
+            if(Mathf.Abs(_vec.x) >= 0.5f || Mathf.Abs(_vec.z) >= 0.5f) {
+                currentSpeed = maxSpeed;
+            }
+            else if(Mathf.Abs(_vec.x) < 0.5f && Mathf.Abs(_vec.z) < 0.5f) {
+                currentSpeed = minSpeed;
+            }
+
+            // カメラの方向から、x-z平面の単位ベクトルを取得
+            Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+            // 方向キーの入力値とカメラの向きから、移動方向の決定
+            Vector3 moveForward = cameraForward * _vec.z + Camera.main.transform.right * _vec.x;
+
+            // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+            velocity = moveForward * currentSpeed + new Vector3(0, velocity.y, 0);
+
+            Debug.Log("移動力 : " + velocity); 
+
+            // キャラクターの向きを進行方向に
+            if (moveForward != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(moveForward);
+            }
         }
 
         /// <summary>
---
+++ Assets/Nakajima/Scripts/TestPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Nakajima.Movement;
+
+/// <summary>
+/// テスト用のプレイヤークラス(消しても問題ない)
+/// </summary>
+public class TestPlayer : MonoBehaviour
+{
+    // 自身のMovement;
+    MovementComponetBase myMovement;
+
+    Rigidbody myRig;
+
+    Vector3 inputVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRig = GetComponent<Rigidbody>();
+        myMovement = GetComponent<MovementComponetBase>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        inputVec.x = Input.GetAxis("Horizontal");
+        inputVec.z = Input.GetAxis("Vertical");
+        
+        myMovement.AddInputVector(inputVec);
+
+        myRig.velocity = myMovement.Velocity;
+    }
+}
