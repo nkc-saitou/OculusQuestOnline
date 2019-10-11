6a0f05c1c0c4bd77cada646c92e3bcb9cb267cb8
Ryo-Matsumoto <nkc.ryo.matsumoto@gmail.com>
2019-10-11 15:46:40 +0900

    [wip]

---
+++ Assets/Matsumoto/Scripts/Utility/IOculusControllable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Matsumoto {
+
+	public interface IOculusControllable {
+	
+		void GetButton(OVRInput.raw)
+
+
+	}
+}
+
+
---
+++ Assets/Matsumoto/Scripts/Weapon/IWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Matsumoto.Weapon {
+
+	public interface IWeapon : IOculusControllable {
+
+	}
+
+
+}
+
