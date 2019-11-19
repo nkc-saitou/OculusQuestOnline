2c19c0be0d9a497950514d2c8bddb2c525e160a8
Ryo-Matsumoto <nkc.ryo.matsumoto@gmail.com>
2019-11-19 15:36:58 +0900

    エフェクトの実装と修正、背景の追加(ignoreしたので各自で追加してください)

--- Assets/Matsumoto/Scripts/Weapon/Object/TestBombObject.cs
+++ Assets/Matsumoto/Scripts/Weapon/Object/TestBombObject.cs
@@ -12,16 +12,24 @@ namespace Matsumoto.Weapon {
 		[SerializeField]
 		private Rigidbody _rigidbody;
 
+		[SerializeField]
+		private ParticleSystem _explosionEffect;
+
 		public override WeaponModuleData ModuleData {
 			get; set;
 		}
-
-
 
 		private void Start() {
 
 			Destroy(gameObject, LifeTime);
 			_rigidbody.AddForce(transform.forward * ModuleData.Speed * Modular.Speed);
+
+		}
+
+		private void OnDestroy() {
+
+			var e = Instantiate(_explosionEffect, transform.position, Quaternion.identity);
+			Destroy(e.gameObject, 10);
 		}
 
 		private void Update() {
