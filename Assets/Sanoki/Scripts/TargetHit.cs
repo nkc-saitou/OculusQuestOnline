22f08fd4ac899f42297980917329838b9b48fcda
sanoki <nkc.hirokazu.sanoki@gmail.com>
2020-01-16 16:49:07 +0900

    ロビーの制作途中

---
+++ Assets/Sanoki/Scripts/TargetHit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sanoki.Lobby
+{
+    public class TargetHit : MonoBehaviour
+    {
+        Animator ani;
+        // Start is called before the first frame update
+        void Start()
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Matsumoto.Weapon.ModuleObject>())
+            {
+                ani.SetTrigger("Hit");
+            }
+        }
+
+
+    }
+}
