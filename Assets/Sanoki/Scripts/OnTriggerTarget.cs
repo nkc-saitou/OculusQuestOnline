8799bc8ffa5d8284ac15f27ae35f912891478b06
sanoki <nkc.hirokazu.sanoki@gmail.com>
2020-01-29 18:56:16 +0900

    Entryアニメーション修正

--- Assets/Sanoki/Scripts/OnTriggerTarget.cs
+++ Assets/Sanoki/Scripts/OnTriggerTarget.cs
@@ -1,26 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class OnTriggerTarget : MonoBehaviour
+namespace Sanoki.Lobby
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    public class OnTriggerTarget : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<Matsumoto.Weapon.ModuleObject>())
+        AniController aniCon;
+        Sanoki.Online.EntrySystem entrySystem;
+        // Start is called before the first frame update
+        void Start()
         {
+            aniCon = FindObjectOfType<AniController>();
+            entrySystem = FindObjectOfType<Sanoki.Online.EntrySystem>();
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                aniCon.AniStop();
+                aniCon.EntryColor();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                aniCon.AniMove();
+                aniCon.ExitColor();
+            }
+
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            Matsumoto.Weapon.ModuleObject obj;
+            obj = other.GetComponent<Matsumoto.Weapon.ModuleObject>();
+            if (TestOnlineData.PlayerID == obj.Owner.GetMyProvider.MyID)
+            {
+                entrySystem.Entry();
+                aniCon.AniStop();
+                aniCon.EntryColor();
+            }
         }
     }
 }
