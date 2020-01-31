c3e71759f3335c7033c65f422dc1b2fc9ddc15c4
chiikama <eru.birdsea@gmail.com>
2020-01-31 12:37:32 +0900

    ガード

---
+++ Assets/chiikama/sprict/Guard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Guard : MonoBehaviour
+{
+    public int HP
+    {
+        get;set;
+    }
+
+    public int MyIndex
+    {
+        get;set;
+    }
+
+    public Action DestroyAct;
+
+    private int currentHP = 0;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Matsumoto.Weapon.ModuleObject>() != null)
+        {
+            currentHP--;
+        }
+
+        if(currentHP <= 0)
+        {
+            Destroy(gameObject);
+            DestroyAct();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/chiikama/sprict/GuardManager.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class GuardManager : MonoBehaviour
+{
+    public GameObject[] GuardPos;
+    public GameObject guardPrefab;
+
+    public int InstantiateCount;
+    public int GuardHP;
+
+    // 今生成されているGuardObjectのIndex
+    List<int> GuardIndex = new List<int>();
+
+    // すべての添字が入った配列
+    List<int> randomIndex = new List<int>();
+
+    List<Guard> CreateGuardList = new List<Guard>();
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    int RandomGuardExtract()
+    {
+        List<int> TMP = new List<int>();
+        
+        // 何も生成されていなければどこのバリアを生成しても良い
+        if(GuardIndex.Count == 0)
+        {
+            TMP.Add(UnityEngine.Random.Range(0, randomIndex.Count));  //無条件でランダム表示する奴
+            Debug.Log("初回 " + TMP[0]);
+            return TMP[0];
+        }
+
+        // ランダムの判定用のリスト
+        List<int> TempIndexLis = new List<int>();
+
+        // すでに生成されているかどうかをチェック
+        for(int i = 0; i < randomIndex.Count; i ++)
+        {
+            if (CheckDuplication(randomIndex[i]))
+            {
+                TempIndexLis.Add(randomIndex[i]);
+            }
+        }
+
+        TMP = TempIndexLis.OrderBy(i => Guid.NewGuid()).Take(1).ToList();
+
+        Debug.Log(TempIndexLis.Count);
+
+        for(int i = 0; i < TempIndexLis.Count; i++)
+        {
+            //Debug.Log(TempIndexLis[i]);
+        }
+
+
+        return TMP[0];
+
+
+    }
+
+    bool CheckDuplication(int _index)
+    {
+        for (int j = 0; j < GuardIndex.Count; j++)
+        {
+            if (_index == GuardIndex[j])
+            {
+                //Debug.Log("重複" + _index);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 6;i++)
+        {
+            randomIndex.Add(i);
+        }
+
+        for(int i = 0; i < InstantiateCount; i++)
+        {
+            GuardIndex.Add(RandomGuardExtract());
+        }
+
+        for(int i = 0; i<GuardIndex.Count;i++)
+        {
+            CreateGuard(GuardIndex[i]);
+        }
+    }
+
+    void GuardChanger(Guard _guard)
+    {
+        _guard.DestroyAct = () => {
+            CreateGuardList.Remove(_guard);
+            GuardIndex.Remove(_guard.MyIndex);
+            GuardIndex.Add(RandomGuardExtract());
+            CreateGuard(GuardIndex[GuardIndex.Count - 1]);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateGuard(int index)
+    {
+        Guard _guard = Instantiate(guardPrefab, GuardPos[index].transform.position, GuardPos[index].transform.rotation).GetComponent<Guard>();
+        _guard.MyIndex = index;
+        CreateGuardList.Add(_guard);
+    }
+
+    //オブジェクトを複数表示
+    //オブジェクトを三回攻撃すると消える
+}
