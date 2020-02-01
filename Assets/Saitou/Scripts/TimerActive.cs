b9d251def5c1812ef6c9ff04ffac485be01d2398
齋藤夕貴 <nkc.yuki.saitou@gmail.com>
2020-02-01 20:43:54 +0900

    ゲームが終わったらタイマーを非表示に

---
+++ Assets/Saitou/Scripts/TimerActive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Nakajima.Main;
+
+namespace Saitou.GameScene
+{
+    public class TimerActive : MonoBehaviour
+    {
+
+        [SerializeField] MainManager _mainManager;
+
+        [SerializeField] GameObject[] _timeUI;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _mainManager.battleEnd = (end) =>
+            {
+                for(int i = 0; i < _timeUI.Length; i++)
+                {
+                    _timeUI[i].SetActive(false);
+                }
+            };
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
